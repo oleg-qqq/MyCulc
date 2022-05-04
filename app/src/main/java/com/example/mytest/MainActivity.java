d87c65f8873c7f2a30a3139e983d7b14919af256
Oleg <2020610@mail.ru>
2022-05-04 13:37:18 +0400

    Немного был изменен дизайн и шрифты калькулятора.

---
+++ app/src/main/java/com/example/mytest/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.mytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private View view; //NET?
+    private int a;
+    private int b;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+
+
+
+
+
+
+
+
+    }
+
+    public void onClick1(View view) {
+    }
+
+    public void onClick2(View view) {
+    }
+
+    public void onClick3(View view) {
+    }
+
+    public void del_onClick(View view) {
+    }
+
+    public void onClick4(View view) {
+    }
+
+    public void onClick5(View view) {
+    }
+
+    public void onClick6(View view) {
+    }
+
+    public void umn_onClick(View view) {
+    }
+
+    public void onClick7(View view) {
+    }
+
+    public void onClick8(View view) {
+    }
+
+    public void onClick9(View view) {
+    }
+
+    public void min_onClick(View view) {
+    }
+
+    public void onClick0(View view) {
+    }
+
+    public void res_onClick(View view) {
+    }
+
+    public void dot_onClick(View view) {
+    }
+
+    public void plus_onClick(View view) {
+    }
+}
